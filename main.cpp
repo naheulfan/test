c54384c88af8217d608eabb26810a6a829b6bb0b
naheulfan <brunopleau@hotmail.com>
2018-04-26 14:50:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 int main(int argc, char* argv)
 {	
 		//HELLO
-		std::out << "Hello fellow human people. Yes yes, I is a human jk I'm a robot" << std::edl;
+		std::out << "Hello fellow human people. Yes yes, I is a human jk I'm a robot jk I'm a lizard" << std::edl;
 		return 0;
 }
