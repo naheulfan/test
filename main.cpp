ff28b00d9e551fec7908895b01848c96a37e1b5c
naheulfan <brunopleau@hotmail.com>
2018-04-26 14:37:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-
+. Just 
 #include <iostream>
 
 int main(int argc, char* argv)
 {	
 		//HELLO
-		std::out << "Hello fellow human people. Yes yes, I is a human" << std::edl;
+		std::out << "Hello fellow human people. Yes yes, I is a human jk I'm a robot" << std::edl;
 		return 0;
 }
